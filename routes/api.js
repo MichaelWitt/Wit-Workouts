3a995c07ee614a5214cfbd4da519e9f9468de834
Annema Cozmoz <mikewittorp@gmail.com>
2021-01-10 21:26:11 -0500

    Updated api routes with get and put requests

--- routes/api.js
+++ routes/api.js
@@ -7,22 +7,39 @@ router.post('/api/workouts', (req, res) => {
 			res.json(dbWorkout);
 		})
 		.catch((err) => {
-			res.status(400).json(err);
+			res.json(err);
 		});
 });
 
 router.get('/api/workouts', (req, res) => {
-	Workout.find()
+	Workout.find({})
+		.limit(7)
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.get('/api/workouts/range', ({ query }, res) => {
+	Workout.find({ day: { $gte: query.start, $lte: query.end } })
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+	Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } }, { new: true, runValidators: true })
 		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
 		.catch((err) => {
-			res.status(400).json(err);
+			res.json(err);
 		});
 });
 
 module.exports = router;
-
-// workout.create;
-// workout.fgindbyidandupdate;
-// workout.find({}).limit(7);
