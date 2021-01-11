81a6e3d766800d219b84d5e2962a8641f0dcb6af
Annema Cozmoz <mikewittorp@gmail.com>
2021-01-11 10:50:36 -0500

    deploying

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 	useFindAndModify: false
 });
 
-// routes
 app.use(require('./routes/api.js'));
 app.use(require('./routes/view.js'));
 
